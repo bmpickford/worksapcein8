1a28135b8fdeccdef8009b90b79de1c99612878c
Benjamin Pickford <b.mpickford@outlook.com>
2022-01-23 10:15:36 +1000

    version fixes

--- out/packages/create_test/index.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { my_package_name } from './index.js';
-
-describe('create_test', () => {
-    it('should return the correct package name', () => {
-        expect(my_package_name).to.equal('create_test');
-    });
-});
