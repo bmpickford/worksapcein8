b1011e71d18f1dd56013c5a000df8327582cdaac
Benjamin Pickford <b.mpickford@outlook.com>
2022-01-30 03:32:47 +1000

    add React support

--- packages/init/create.js
+++ packages/init/create.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import chalk from 'chalk';
 import { PackageManager } from '@workspacein8/packagemanager';
 import { create as createWorkspace } from '@workspacein8/workspace';
-import { create as createPackage } from '@workspacein8/package';
+import { create as createPackage, createComponent as createComponentPackage } from '@workspacein8/package';
 import { execSync } from 'child_process';
 
 /**
@@ -42,36 +42,61 @@ export default async function create(opts) {
   PackageManager.cwd = opts.out_dir;
   PackageManager.debug = opts.debug || false;
 
-  if (opts.debug)
+  if (opts.debug) {
     console.debug(
       `${chalk.blue('Your options: ')}${chalk.blue(
         JSON.stringify(opts, null, 2)
       )}`
     );
-
-  fs.mkdirSync(`${opts.out_dir}`);
-  if (!fs.existsSync(`${opts.out_dir}/packages`))
-    fs.mkdirSync(`${opts.out_dir}/packages`);
-  if (!fs.existsSync(`${opts.out_dir}/packages/${opts.package_name}`))
-    fs.mkdirSync(`${opts.out_dir}/packages/${opts.package_name}`);
-  await Promise.all([
-    createWorkspace(opts),
-    createPackage({
-      ...opts,
-      out_dir: `${opts.out_dir}/packages/${opts.package_name}`,
-    }),
-  ]);
-  try {
-    execSync(
-      `npx npx -p typescript tsc ${opts.out_dir}/packages/${opts.package_name}/index.js --declaration --emitDeclarationOnly --allowJs`
-    );
-    console.log(`${chalk.green('✓')} Created type definition`);
-  } catch (error) {
-    console.error(chalk.red(error));
   }
 
+  initDirs(opts);
+  initGit(opts);
+  
+  await createWorkspace(opts);
+  await createPackageDir(opts);
+  
   addDeps(opts);
   addExample(opts);
+}
+
+/**
+ * Adds initial dirs
+ *
+ * @param {CreateOpts} opts
+ */
+function initDirs(opts) {
+  fs.mkdirSync(`${opts.out_dir}`);
+  if (!fs.existsSync(`${opts.out_dir}/packages`)) {
+    fs.mkdirSync(`${opts.out_dir}/packages`);
+  }
+  if (!fs.existsSync(`${opts.out_dir}/packages/${opts.package_name}`)) {
+    fs.mkdirSync(`${opts.out_dir}/packages/${opts.package_name}`);
+  }
+}
+
+function initGit(opts) {
+  execSync('git init', { cwd: opts.out_dir});
+  execSync('git checkout -b main', { cwd: opts.out_dir});
+}
+
+/**
+ * Creates packages
+ *
+ * @param {CreateOpts} opts
+ */
+async function createPackageDir(opts) {
+  if (opts.template_type === 'components') {
+    await createComponentPackage({
+      ...opts,
+      out_dir: `${opts.out_dir}/packages/${opts.package_name}`,
+    });
+  } else {
+    await createPackage({
+      ...opts,
+      out_dir: `${opts.out_dir}/packages/${opts.package_name}`,
+    });
+  }
 }
 
 /**
@@ -84,11 +109,32 @@ function addDeps(opts) {
   let out = PackageManager.run(opts.workspace_type).addDev(
     'eslint vite jsdoc mocha chai'
   );
+
   if (opts.typescript || opts.docs) {
     out += PackageManager.run(opts.workspace_type).addDev('typescript typedoc');
+    addTypeDecleration(opts);
   }
-  if (opts.debug) console.log(`${chalk.blue(out?.toString().trim())}`);
+
+  if (opts.template_type === 'components') {
+    // out += PackageManager.run(opts.workspace_type).addDev('typescript typedoc');
+  }
+
+  if (opts.debug) {
+    console.log(`${chalk.blue(out?.toString().trim())}`);
+  }
   console.log(`${chalk.green('✓')} Installed dependencies!`);
+}
+
+function addTypeDecleration(opts) {
+  try {
+    execSync(
+      `npx npx -p typescript tsc ${opts.out_dir}/packages/${opts.package_name}/index.js --declaration --emitDeclarationOnly --allowJs`,
+      { cwd: opts.out_dir }
+    );
+    console.log(`${chalk.green('✓')} Created type definition`);
+  } catch (error) {
+    console.error(chalk.red(error));
+  }
 }
 
 /**
--- packages/package/index.js
+++ packages/package/index.js
@@ -2,6 +2,8 @@ import chalk from 'chalk';
 import { CorePackageGenerator } from '@workspacein8/core';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { PackageManager } from '@workspacein8/packagemanager';
+import { readFileSync, writeFileSync } from 'fs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -52,21 +54,40 @@ export async function createComponent(
   type = 'button'
 ) {
   const pkg = new PackageGenerator(opts);
+  sbInit(opts, framework);
 
   const filetype = opts.typescript ? 'ts' : 'js';
   await Promise.all([
-    pkg.generate('package.json'),
+    pkg.generate('package.json', 'package.json', `${framework}/common/`),
     pkg.generate('README.md'),
     pkg.generate(
-      'index.js',
-      `index.${filetype}`,
+      'index.jsx',
+      `index.${filetype}x`,
       `${framework}/components/${type}`
     ),
     pkg.generate(
-      'index.story.js',
-      `index.story.${filetype}`,
+      'index.stories.jsx',
+      `index.stories.${filetype}x`,
+      `${framework}/components/${type}`
+    ),
+    pkg.generate(
+      'index.module.css',
+      'index.module.css',
       `${framework}/components/${type}`
     ),
   ]);
   console.log(`${chalk.green('✓')} Generated ${type} files`);
+}
+
+function sbInit(opts, framework = 'react') {
+  console.log('Generating storybook, this may take a couple minutes..');
+  PackageManager.run(opts.workspace_type).exec(`npx sb init --yes --type ${framework} --builder webpack5 --skip-install`);
+  PackageManager.run(opts.workspace_type).exec(`npx rimraf stories`);
+  
+  const sbFile = `${opts.out_dir}/../../.storybook/main.js`;
+  const mainjs = readFileSync(sbFile, 'utf8');
+  const out = mainjs.replace(/stories\//g, '');
+  writeFileSync(sbFile, out, 'utf8');
+
+  console.log(`${chalk.green('✓')} Added storybook`);
 }
